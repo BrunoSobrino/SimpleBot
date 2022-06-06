c6e0b604feb637ed1076ed9146c242086fba2c47
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-06-06 03:30:16 -0500

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 let express = require('express')
 let path = require('path')
+let { createServer } = require('http')
 let SocketIO = require('socket.io')
 let qrcode = require('qrcode')
+let fetch = require('node-fetch')
 
 function connect(conn, PORT) {
     let app = global.app = express()
-
-    app.use(express.static(path.join(__dirname, 'views')))
+    console.log(app)
+    let server = global.server = createServer(app)
+    // app.use(express.static(path.join(__dirname, 'views')))
     let _qr = 'invalid'
+
+    conn.ev.on('connection.update', function appQR({ qr }) {
+        if (qr) _qr = qr
+    })
+
     app.use(async (req, res) => {
         res.setHeader('content-type', 'image/png')
-        res.end(await qrcode.toBuffer(_qr))
+        res.end(await toBuffer(_qr))
     })
-    conn.on('qr', qr => {
-        _qr = qr
-    })
-    
-    let server = app.listen(PORT, () => console.log('App listened on port', PORT))
-    let io = SocketIO(server)
-    io.on('connection', socket => {
-        let { unpipeEmit } = pipeEmit(conn, socket, 'conn-')
-        socket.on('disconnect', unpipeEmit)
+
+    // let io = new Socket(server)
+    // io.on('connection', socket => {
+    //     let { unpipeEmit } = pipeEmit(conn, socket, 'conn-')
+    //     socket.on('disconnect', unpipeEmit)
+    // })
+
+    server.listen(PORT, () => {
+        console.log('App listened on port', PORT)
+        if (opts['keepalive']) keepAlive()
     })
 }
 
@@ -37,5 +46,12 @@ function pipeEmit(event, event2, prefix = '') {
     }
 }
 
+function keepAlive() {
+    const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
+    if (/(\/\/|\.)undefined\./.test(url)) return
+    setInterval(() => {
+        fetch(url).catch(console.error)
+    }, 5 * 1000 * 60)
+}
 
 module.exports = connect
