ff1109dc95f439c8952eb0f95921b09e31665e4f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-06-06 19:48:55 -0500

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/random_dog.js
+++ plugins/random_dog.js
@@ -1,25 +1,19 @@
-//created by Scooppt
 let fetch = require('node-fetch')
-
 let handler  = async (m, { conn, text }) => {
-  try {
-    let res = await fetch('https://random.dog/woof.json')
-    let json = await res.json()
-    if (json.status) throw json
-    let caption = `
-© Chatbot
+try {
+let res = await fetch('https://random.dog/woof.json')
+let json = await res.json()
+if (json.status) throw json
+let caption = `
+*©𝑆𝑖𝑚𝑝𝑙𝑒𝐵𝑜𝑡*
 `.trim()
-    conn.sendFile(m.chat, json.url, 'dog.jpg', caption, m)
-   } catch (e) {
-        console.log(e)
-        throw '_*Erro!*_'
-    }
-}
-
+conn.sendFile(m.chat, json.url, 'dog.jpg', caption, m)
+} catch (e) {
+console.log(e)
+throw '*[❗] 𝙴𝚁𝚁𝙾𝚁*'
+}}
 handler.help = ['dog']
 handler.tags = ['general']
 handler.command = /^dog$/i
-
 handler.fail = null
-
 module.exports = handler
