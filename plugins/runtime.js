8ca196fbd767d731cfd494a90a4a7203460fc3d0
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-06-06 19:53:21 -0500

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -1,22 +1,17 @@
 let handler = async (m, { conn }) => {
-    let _uptime = process.uptime() * 1000
-    let uptime = clockString(_uptime)
-
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
 m.reply(`
-┌─〔 R U N T I M E 〕
-├ Bot Aktif Selama ${uptime}
-└────
-    `.trim())
-}
+*┌─〔 ＡＣＴＩＶＯ 〕*
+*├ 𝑇𝑖𝑒𝑚𝑝𝑜 𝐴𝑐𝑡𝑖𝑣𝑜: ${uptime}*
+*└───────────────*
+`.trim())}
 handler.help = ['runtime']
 handler.tags = ['info']
 handler.command = /^(uptime|runtime)$/i
-
 module.exports = handler
-
 function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
