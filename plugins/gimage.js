116be99b3f4ba7060b84147f86519692cedc5813
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-06-06 19:37:58 -0500

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -1,23 +1,18 @@
 let { promisify } = require('util')
 let _gis = require('g-i-s')
 let gis = promisify(_gis)
-
 let handler  = async (m, { conn, args, text }) => {
-  if (!text) return m.reply('Cari apa?')
-  let results = await gis(text) || []
-  let { url, width, height } = pickRandom(results) || {}
-  if (!url) return m.reply('404 Not Found')
-  conn.sendFile(m.chat, url, 'gimage', `
+if (!text) return m.reply('Cari apa?')
+let results = await gis(text) || []
+let { url, width, height } = pickRandom(results) || {}
+if (!url) return m.reply('*[❗] 𝚂𝙴𝚁𝚅𝙸𝙳𝙾𝚁 𝙲𝙰𝙸𝙳𝙾, 𝙸𝙽𝚃𝙴𝙽𝚃𝙴 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*')
+conn.sendFile(m.chat, url, 'gimage', `
 🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}
 🌎 *𝙱𝚄𝙲𝙰𝙳𝙾𝚁:* Google
-`.trim(), m)
-}
+`.trim(), m)}
 handler.help = ['gimage <query>', 'image <query>']
 handler.tags = ['general']
 handler.command = /^(gimage|image|imagen)$/i
-
 module.exports = handler
-
 function pickRandom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)]
-}
+return arr[Math.floor(Math.random() * arr.length)]}
