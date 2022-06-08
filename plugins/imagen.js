45f1f8746831135e54df984ed71b482484063997
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-06-08 05:52:35 -0500

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/imagen.js
+++ plugins/imagen.js
@@ -2,7 +2,7 @@ let { promisify } = require('util')
 let _gis = require('g-i-s')
 let gis = promisify(_gis)
 let handler  = async (m, { conn, args, text }) => {
-if (!text) return m.reply('Cari apa?')
+if (!text) return m.reply('*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*')
 let results = await gis(text) || []
 let { url, width, height } = pickRandom(results) || {}
 if (!url) return m.reply('*[❗] 𝚂𝙴𝚁𝚅𝙸𝙳𝙾𝚁 𝙲𝙰𝙸𝙳𝙾, 𝙸𝙽𝚃𝙴𝙽𝚃𝙴 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*')
