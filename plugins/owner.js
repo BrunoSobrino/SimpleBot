edb56b55e5d49da20608edb0b17a1c1c10a2b3ef
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-22 13:09:57 -0500

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,4 @@
+function handler(m) {
+this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)}
+handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
+module.exports = handler
