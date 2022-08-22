3dc3a6654926146a408bdc3027067ba0f30964cc
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-22 13:10:19 -0500

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,4 +1,5 @@
 function handler(m) {
-this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)}
+this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
+}
 handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
 module.exports = handler
