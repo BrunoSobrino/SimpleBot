2e261d082c6a654f7a290024816e65ea4c93c1c4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-22 13:13:49 -0500

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- config.js
+++ config.js
@@ -1,6 +1,20 @@
+//============= Datos del Bot =============//
+
+global.owner = ['5219996125657']
+global.packname = '(☞ﾟ∀ﾟ)☞'
+global.author = '𝕊𝕚𝕞𝕡𝕝𝕖𝔹𝕠𝕥'
+global.wm = '𝕊𝕚𝕞𝕡𝕝𝕖𝔹𝕠𝕥'
+global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
+
+//============= urlButtons =============//
+
+global.textUrlbot = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
+global.linkUrlbot = "https://www.instagram.com/ilmanhdyt_"
+
+//=========================================================================//
+
 global.DeveloperMode = 'false' 
 global.linkGC = []
-global.owner = ['5219996125657']
 global.mods = [] 
 global.prems = [] 
 global.APIs = { 
@@ -12,10 +26,6 @@ zahir: 'https://zahirr-web.herokuapp.com' }
 global.APIKeys = { 
 'https://api.xteam.xyz': 'd90a9e986e18778b',
 'https://zahirr-web.herokuapp.com': 'zahirgans' }
-global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = '𝕊𝕚𝕞𝕡𝕝𝕖𝔹𝕠𝕥'
-global.wm = '𝕊𝕚𝕞𝕡𝕝𝕖𝔹𝕠𝕥'
-global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
 global.lolkeysapi = ['85faf717d0545d14074659ad']
 global.keysZens = ['fiktod', 'c2459db922', 'BF39D349845E', '675e34de8a', '37CC845916', '0b917b905e6f', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
