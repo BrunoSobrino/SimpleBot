f252386f1831c4233905a03a5f83f4d9b7ee8360
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-26 12:17:57 -0500

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/getvid.js
+++ plugins/getvid.js
@@ -1,13 +1,12 @@
+let fetch = require('node-fetch')
 let { youtubedl, youtubedlv2 } = require('@bochilteam/scraper')
 let fs = require('fs')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
 let qu = args[1] || '360'
 let q = qu + 'p'
 let v = args[0]
-let _thumb = {}
-catch (e) { }
 const yt = await youtubedl(v).catch(async () => await  youtubedlv2(v))
-try { _thumb = { jpegThumbnail: await (await fetch(yt.thumbnail)).buffer() } }
+let _thumb = { jpegThumbnail: await (await fetch(yt.thumbnail)).buffer() }
 const dl_url = await yt.video[q].download()
 const ttl = await yt.title
 const size = await yt.video[q].fileSizeH
