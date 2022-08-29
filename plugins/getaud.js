ddee5d5e9d178980838dcc71878d39c405e4bd34
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-28 23:01:36 -0500

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/getaud.js
+++ plugins/getaud.js
@@ -1,12 +1,17 @@
 let { youtubedl, youtubedlv2 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
+if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*'
+await m.reply(global.await)
+try {
 let q = '128kbps'
 let v = args[0]
-const yt = await youtubedl(v).catch(async () => await  youtubedlv2(v))
+const yt = await youtubedl(v).catch(async () => await youtubedlv2(v)).catch(async () => await youtubedlv3(v))
 const dl_url = await yt.audio[q].download()
 const ttl = await yt.title
 const size = await yt.audio[q].fileSizeH
-await m.reply(`▢ Tɪᴛᴛʟᴇ: ${ttl}\n▢  Sɪᴢᴇ: ${size}\n\n▢ Ｌｏａｄｉｎｇ. . .`)
-await conn.sendFile(m.chat, dl_url, ttl + '.mp3', null, m, null, { asDocument: false })}
+await conn.sendFile(m.chat, dl_url, ttl + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
+} catch {
+await conn.reply(m.chat, '*[❗] 𝙴𝚁𝚁𝙾𝚁 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝙰𝚄𝙳𝙸𝙾*', m)}
+}  
 handler.command = /^(getaud|ytmp3.2|yta.2)$/i
 module.exports = handler
