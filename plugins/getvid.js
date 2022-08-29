6e955c9e9ddfafabe568a657e112e5b2fc0e7069
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-29 00:47:28 -0500

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/getvid.js
+++ plugins/getvid.js
@@ -3,7 +3,7 @@ let { youtubedl, youtubedlv2, youtubedlv3 } = require('@bochilteam/scraper')
 let fs = require('fs')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
 if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*'
-await m.reply(global.await)
+await m.reply(global.wait)
 try {
 let qu = args[1] || '360'
 let q = qu + 'p'
