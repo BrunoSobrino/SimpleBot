b935a5de75b28d5e0edf4083dbbff877ed95b0d5
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-02 01:05:12 -0500

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/infohost.js
+++ plugins/infohost.js
@@ -1,6 +1,20 @@
 let handler = async (m, { conn }) => {
 m.reply(`
-En desarrollo 
+*Hola! Buscas la mejor alternativa a Heroku, termux, replit, etc. de manera gratuita? 
+
+*- Esta es tu mejor opción para tener tu Bot o algun proyecto activo 24/7*
+
+*Incluso mejor que otros servicios como los antes mencionados, ¿Muy lento tu nokia y necesitas tener activado tu bot 24/7?*
+
+*Entra a este grupo y pide mayor información, con gusto te ayudamos a ejecutar tu Bot en el Host y olvidate de lag y falta de memoria.*
+
+*[_Pagina principal_]*
+
+- acidicnodes.com
+
+*[_Grupo Oficial_]*
+
+- https://chat.whatsapp.com/F0fU7LSlBBcBm6ny5fVSuT
 `)
 }
 handler.command = /^(infohost)$/i
