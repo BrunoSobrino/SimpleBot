9ff952b4a3c49381aee47ba7d47ba2055cc27e2b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-21 06:22:33 -0600

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/getaud.js
+++ plugins/getaud.js
@@ -1,8 +1,13 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 let fetch = require('node-fetch')
+let { ytmp4, ytmp3, ytplay, ytplayvid } = require('../lib/youtube')
 let { youtubedl, youtubedlv2, youtubedlv3 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, command, text, args }) => {
 if (!text) return m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*`)
+try {
+let AAaaAA = await ytmp3(args[0])  
+conn.sendMessage(m.chat, { audio: { url: AAaaAA.result }, fileName: `error.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
+} catch {  
 try {
 let q = '128kbps'
 let v = args[0]
@@ -19,6 +24,6 @@ let n = lolh.result.title || 'error'
 await conn.sendMessage(m.chat, { audio: { url: lolh.result.link }, fileName: `${n}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
 } catch {
 m.reply(`*[❗] 𝙴𝚁𝚁𝙾𝚁 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝙰𝚄𝙳𝙸𝙾*`)
-}}}
+}}}}
 handler.command = /^(getaud|ytmp3.2|yta.2|ytmp3|yta)$/i
 module.exports = handler
