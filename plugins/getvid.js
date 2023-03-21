e8c565a42237e920633e2dec7f796fea29fe50f3
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-21 06:18:11 -0600

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/getvid.js
+++ plugins/getvid.js
@@ -4,6 +4,10 @@ let { youtubedl, youtubedlv2, youtubedlv3 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, command, text, args }) => {
 if (!text) return m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*`)
 try {
+let AAaa = await ytmp4(args[0])  
+conn.sendMessage(m.chat, { video: { url: AAaa.result }, fileName: `error.mp4`, thumbnail: AAaa.thumbnail, mimetype: 'video/mp4' }, { quoted: msg })  
+} catch {  
+try {
 let qu = args[1] || '360'
 let q = qu + 'p'
 let v = args[0]
@@ -23,6 +27,6 @@ let n4 = lolh.result.thumbnail
 await conn.sendMessage(m.chat, { video: { url: n2 }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `▢ 𝚃𝙸𝚃𝚄𝙻𝙾: ${n}\n▢ 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾: ${n3}`, thumbnail: await fetch(n4) }, { quoted: m })
 } catch {
 m.reply(`*[❗] 𝙴𝚁𝚁𝙾𝚁 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾*`)
-}}}
+}}}}
 handler.command = /^(getvid|ytmp4.2|ytv.2|ytv|ytmp4)$/i
 module.exports = handler
