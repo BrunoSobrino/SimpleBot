ba096e711fce08d9a99c0eecf6c02446a4d4f3b4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-21 16:46:55 -0600

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,34 +1,6 @@
-let axios = require("axios")
+let hispamemes  = require("hispamemes ")
 let handler = async (m, {command, conn}) => {
-let res = await axios(pickRandom(meme))
-let json = res.data
-let url = json.url
+const url = await hispamemes.meme() 
 conn.sendButtonImg(m.chat, url, `_${command}_`.trim(), author, '🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`, m)}
 handler.command = /^(meme)$/i
 module.exports = handler
-
-function pickRandom(list) {
-return list[Math.floor(list.length * Math.random())]}
-
-const meme = [
-"https://meme-api.herokuapp.com/gimme/memesmexico",
-"https://meme-api.herokuapp.com/gimme/mememexico",
-"https://meme-api.herokuapp.com/gimme/memeslatam",
-"https://meme-api.herokuapp.com/gimme/memeslatinoamerica",
-"https://meme-api.herokuapp.com/gimme/latammemes",
-"https://meme-api.herokuapp.com/gimme/memeslatinoamerica",
-"https://meme-api.herokuapp.com/gimme/latammemes",
-"https://meme-api.herokuapp.com/gimme/memesmexico",
-"https://meme-api.herokuapp.com/gimme/mememexico",
-"https://meme-api.herokuapp.com/gimme/memeslatam",
-"https://meme-api.herokuapp.com/gimme/memesmexico",
-"https://meme-api.herokuapp.com/gimme/mememexico",
-"https://meme-api.herokuapp.com/gimme/memeslatam",
-"https://meme-api.herokuapp.com/gimme/memeslatinoamerica",
-"https://meme-api.herokuapp.com/gimme/latammemes",
-"https://meme-api.herokuapp.com/gimme/memeslatinoamerica",
-"https://meme-api.herokuapp.com/gimme/latammemes",
-"https://meme-api.herokuapp.com/gimme/memesmexico",
-"https://meme-api.herokuapp.com/gimme/mememexico",
-"https://meme-api.herokuapp.com/gimme/memeslatam"
-]
