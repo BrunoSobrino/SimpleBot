c69e02296d1b3f77eb9e61913fc2328646ef5c13
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-21 14:55:33 -0600

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/igdl.js
+++ plugins/igdl.js
@@ -1,11 +1,31 @@
 let fetch = require('node-fetch')
-let instagramGetUrl = require('fg-ig')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!(args[0])) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
-let results = (await instagramGetUrl(args[0])).url_list[0]
-let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${results}`)).text()
-let txt = `🔗 *Url:* ${shortUrl}`.trim()
-await conn.sendFile(m.chat, results, 'error.mp4', txt, m)}
+const instagramGetUrl = require('instagram-url-direct')
+const { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link` 
+m.reply(`*[❗] ᴅᴇsᴄᴀʀɢᴀɴᴅᴏ sᴜ ᴠɪᴅᴇᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴘᴏʀ ғᴀᴠᴏʀ, ᴇsᴛᴇ ᴘʀᴏᴄᴇsᴏ ᴘᴜᴇᴅᴇ ᴅᴜʀᴀʀ ᴇɴᴛʀᴇ 2 ʏ 10 ᴍɪɴᴜᴛᴏs ᴅᴇᴘᴇɴᴅɪᴇɴᴅᴏ ᴅᴇ ʟᴀ ᴅᴜʀᴀᴄɪᴏɴ ᴅᴇʟ ᴠɪᴅᴇᴏ...*`)
+try {
+let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
+let json = await human.json()
+let videoig = json.result
+let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
+await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)
+} catch {
+try {  
+const resultss = (await instagramGetUrl(args[0])).url_list[0]
+let shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt2 = `🔗 *Url:* ${shortUrl2}`.trim()
+conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m)
+} catch {
+try {  
+const resultssss = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0])).catch(async _ => await instagramdlv3(args[0])).catch(async _ => await instagramdlv4(args[0]))
+let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
+let txt3 = `🔗 *Url:* ${shortUrl3}`.trim()
+for (const { url } of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m)
+break       
+} catch {
+await await m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾, 𝚂𝙸 𝙴𝙻 𝙴𝚁𝚁𝙾𝚁 𝚂𝙸𝙶𝚄𝙴, 𝙿𝚁𝚄𝙴𝙱𝙴 𝙲𝙾𝙽 𝙾𝚃𝚁𝙰 𝙾𝙿𝙲𝙸𝙾𝙽 (${usedPrefix}ig, ${usedPrefix}ig2, ${usedPrefix}ig3)*`)
+}}}}}
 handler.command =/^(instagram|ig(dl)?)$/i
-handler.dfail = null
 module.exports = handler
