7c01c6d313da4b3ee6f23da491c496098ab7076c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-21 16:47:16 -0600

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,4 +1,4 @@
-let hispamemes  = require("hispamemes ")
+let hispamemes = require("hispamemes")
 let handler = async (m, {command, conn}) => {
 const url = await hispamemes.meme() 
 conn.sendButtonImg(m.chat, url, `_${command}_`.trim(), author, '🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`, m)}
