d7e15eae7539549becd3c2529918631939abbc88
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-21 14:06:41 -0600

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/google.js
+++ plugins/google.js
@@ -1,5 +1,6 @@
 const { googleIt } = require('@bochilteam/scraper')
 const fetch = require('node-fetch')
+const axios = require('axios')
 let handler = async (m, { conn, command, args }) => {
 //const fetch = (await import('node-fetch')).default
 let text = args.join` `
@@ -8,8 +9,9 @@ let url = 'https://google.com/search?q=' + encodeURIComponent(text)
 let search = await googleIt(text)
 let msg = search.articles.map(({ title, url, description }) => { return `*${title}*\n_${url}_\n_${description}_` }).join('\n\n')
 try {
-let ss = await (await fetch(`https://api.lolhuman.xyz/api/ssweb?apikey=${lolkeysapi}&url=${url}`)).arrayBuffer()
-await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m)
+let ss = await ssweb(url, 'desktop')
+//let ss = await (await fetch(`https://api.lolhuman.xyz/api/ssweb?apikey=${lolkeysapi}&url=${url}`)).arrayBuffer()
+await conn.sendFile(m.chat, ss.result, 'error.png', url + '\n\n' + msg, m)
 } catch {
 m.reply(msg)
 }}
@@ -17,3 +19,40 @@ handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
 handler.tags = ['internet']
 handler.command = /^googlef?$/i
 module.exports = handler
+
+async function ssweb(url, device = 'desktop'){
+     return new Promise((resolve, reject) => {
+          const base = 'https://www.screenshotmachine.com'
+          const param = {
+            url: url,
+            device: device,
+            cacheLimit: 0
+          }
+          axios({url: base + '/capture.php',
+               method: 'POST',
+               data: new URLSearchParams(Object.entries(param)),
+               headers: {
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+               }
+          }).then((data) => {
+               const cookies = data.headers['set-cookie']
+               if (data.data.status == 'success') {
+                    axios.get(base + '/' + data.data.link, {
+                         headers: {
+                              'cookie': cookies.join('')
+                         },
+                         responseType: 'arraybuffer'
+                    }).then(({ data }) => {
+                       let result = {
+                            status: 200,
+                            author: 'Ryzn',
+                            result: data
+                        }
+                         resolve(result)
+                    })
+               } else {
+                    reject({ status: 404, author: 'Ryzn', message: data.data })
+               }
+          }).catch(reject)
+     })
+}
