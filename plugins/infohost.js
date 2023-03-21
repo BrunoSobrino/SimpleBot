fa9346ccef1fd8b191f73ec4f1c5b7ee6ef7727f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-21 05:31:13 -0600

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/infohost.js
+++ plugins/infohost.js
@@ -1,14 +1,21 @@
 let handler = async (m, { conn }) => {
-m.reply(`
-*Hola! Buscas la mejor alternativa a Heroku, termux, replit, etc. de manera gratuita?*
+m.reply(`*Hola! Buscas la mejor alternativa a Heroku, termux, replit, etc. de manera gratuita o de paga?*
 
-*- Esta es tu mejor opción para tener tu Bot o algun proyecto activo 24/7*
+*- Esta es tu mejor opción para tener tu Bot, pagina web, servidor de minecraft o algun otro proyecto activo 24/7*
 
 *Incluso mejor que otros servicios como los antes mencionados, ¿Muy lento tu nokia y necesitas tener activado tu bot 24/7?*
 
 *[_Pagina principal_]*
 
-- boxmineworld.com`)
+- boxmineworld.com
+
+*[_BoxMine Dash_]*
+
+- dash.boxmineworld.com
+
+*[_BoxMine Panel_]*
+
+- panel.boxmineworld.com`trim())
 }
 handler.command = /^(infohost)$/i
 module.exports = handler
