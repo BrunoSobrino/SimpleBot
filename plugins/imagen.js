14aaa49c81d868b7a9c7ef556b6e4915c5869fb9
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-21 16:44:26 -0600

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/imagen.js
+++ plugins/imagen.js
@@ -2,7 +2,7 @@ const { googleImage } = require('@bochilteam/scraper')
 let handler  = async (m, { conn, args, text }) => {
 if (!text) return m.reply('*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*')
 const res = await googleImage(text)
-let image = await res.getRandom()
+let image = res[Math.floor(Math.random() * res.length)]
 let link = image
 if (!link) return m.reply('*[❗] 𝚂𝙴𝚁𝚅𝙸𝙳𝙾𝚁 𝙲𝙰𝙸𝙳𝙾, 𝙸𝙽𝚃𝙴𝙽𝚃𝙴 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*')
 conn.sendFile(m.chat, link, 'gimage', `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}\n🌎 *𝙱𝚄𝙲𝙰𝙳𝙾𝚁:* Google`.trim(), m)}
@@ -10,5 +10,3 @@ handler.help = ['gimage <query>', 'image <query>']
 handler.tags = ['general']
 handler.command = /^(gimage|image|imagen)$/i
 module.exports = handler
-function pickRandom(arr) {
-return arr[Math.floor(Math.random() * arr.length)]}
