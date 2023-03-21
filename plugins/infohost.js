a3cdf0ff3c572f450dfaba3732e249a10d6dfc92
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-20 19:53:42 -0600

    💫 𝑆𝐼𝑀𝑃𝐿𝐸 - 𝐵𝑂𝑇 💫

--- plugins/infohost.js
+++ plugins/infohost.js
@@ -1,18 +1,14 @@
 let handler = async (m, { conn }) => {
 m.reply(`
-*Hola! Buscas la mejor alternativa a Heroku, termux, replit, etc. de manera gratuita? 
+*Hola! Buscas la mejor alternativa a Heroku, termux, replit, etc. de manera gratuita?*
 
 *- Esta es tu mejor opción para tener tu Bot o algun proyecto activo 24/7*
 
 *Incluso mejor que otros servicios como los antes mencionados, ¿Muy lento tu nokia y necesitas tener activado tu bot 24/7?*
 
-*Entra a este grupo y pide mayor información, con gusto te ayudamos a ejecutar tu Bot en el Host y olvidate de lag y falta de memoria.*
-
 *[_Pagina principal_]*
 
-- boxmineworld.com
-
-*[_Grupo Oficial_]*`)
+- boxmineworld.com`)
 }
 handler.command = /^(infohost)$/i
 module.exports = handler
